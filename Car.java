0195a971eb57bc71b426b2ea44ce4960926a6578
hronorog <hronorog@gmail.com>
2023-03-16 23:43:15 +0300

    #16 - abstract

--- Car.java
+++ Car.java
@@ -5,4 +5,15 @@ public class Car extends Transport {
     public Car (float speed, int weight, String color, byte[] coordinate) {
         super(speed, weight, color, coordinate);
     }
+
+    @Override
+    public void moveObject(float speed) {
+        System.out.println("Наш объект двигается со скоростью: " + speed);
+    }
+
+    @Override
+    public boolean stopObject() {
+        this.speed = 0;
+        return true;
+    }
 }
--- Truck.java
+++ Truck.java
@@ -13,6 +13,17 @@ public class Truck  extends Transport{
         this.isLoaded = isLoaded;
     }
 
+    @Override
+    public void moveObject(float speed) {
+        System.out.println("Наш объект двигается со скоростью: " + speed);
+    }
+
+    @Override
+    public boolean stopObject() {
+        this.speed = 0;
+        return true;
+    }
+
     public void setValues(float speed, int weight, String color, byte[] coordinate, boolean isLoaded) {
         super.setValues(speed, weight, color, coordinate);
         this.isLoaded = isLoaded;
